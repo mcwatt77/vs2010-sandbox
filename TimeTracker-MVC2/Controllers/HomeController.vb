3a37e787d088345bcc263809811263eb711a09c8
Darrel Lee <dlee@resdat.com>
2011-07-24 22:23:07 -0800

    Update welcome message.

--- TimeTracker-MVC2/Controllers/HomeController.vb
+++ TimeTracker-MVC2/Controllers/HomeController.vb
@@ -3,7 +3,7 @@ Public Class HomeController
     Inherits System.Web.Mvc.Controller
 
     Function Index() As ActionResult
-        ViewData("Message") = "Welcome to ASP.NET MVC!"
+        ViewData("Message") = "Welcome to TimeTracker"
 
         Return View()
     End Function
