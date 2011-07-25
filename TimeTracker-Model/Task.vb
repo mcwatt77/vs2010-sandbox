95cdb006455d44db7f78a1852a5d2cbca77b038b
Darrel Lee <dlee@resdat.com>
2011-07-24 22:18:32 -0800

    Add Task model and App_Notes. (Must also include .vbproj file)

---
+++ TimeTracker-Model/Task.vb
@@ -0,0 +1,14 @@
+﻿Namespace TimeTracker.Model
+    <Serializable()>
+    Public Class Task
+
+        Public Property TaskId As Integer
+        Public Property Description As String
+
+        Public Property AgentId As Integer
+        Public Property WorkItemId As Integer
+        Public Property GroupId As Integer
+
+    End Class
+
+End Namespace
