98f67a1d4fe56782dcd99db41c6895e890d6b239
Darrel Lee <dlee@resdat.com>
2011-12-02 22:36:03 -0900

    Add wpf sandbox

---
+++ Wpf-Sandbox/Application.xaml.vb
@@ -0,0 +1,24 @@
+﻿Class Application
+    ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
+    ' can be handled in this file.
+
+    Dim logger As New TextWriterTraceListener("C:\AppLogs\application.log")
+
+    Private Sub Application_DispatcherUnhandledException(sender As Object, e As System.Windows.Threading.DispatcherUnhandledExceptionEventArgs) Handles Me.DispatcherUnhandledException
+        logger.WriteLine(e.Exception.Message)
+    End Sub
+
+    Private Sub Application_Startup(sender As Object, e As System.Windows.StartupEventArgs) Handles Me.Startup
+        logger.WriteLine("Application Start")
+        Trace.AutoFlush = True
+        Trace.Listeners.Add(logger)
+    End Sub
+
+    Private Sub Application_Exit(sender As Object, e As System.Windows.ExitEventArgs) Handles Me.Exit
+        logger.WriteLine("Application Exit")
+        logger.Close()
+        Trace.Listeners.Remove(logger)
+    End Sub
+
+
+End Class
---
+++ Wpf-Sandbox/MainWindow.xaml.vb
@@ -0,0 +1,17 @@
+﻿Class MainWindow 
+
+    Private LastClick As DateTime = DateTime.MinValue
+
+    Private Sub DefaultButton_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles DefaultButton.Click
+        Dim btn As Button = sender
+        Dim now As DateTime = DateTime.Now
+        Dim diff As TimeSpan = TimeSpan.Zero
+        If LastClick <> DateTime.MinValue Then
+            diff = (now - LastClick)
+        End If
+        LastClick = now
+        Dim msg = String.Format("{1} Elapsed: {2}", btn.Name, now, diff)
+        Trace.WriteLine(msg)
+    End Sub
+
+End Class
